62340809a1764d9417532a06eb949e0456b836eb
SurajSakhare100 <sakharesuraj10@gmail.com>
2025-01-22 12:02:29 +0530

    Basic Java Done

---
+++ Learn/Basics/Conditional Statements/ternary_operator.java
@@ -0,0 +1,7 @@
+public class ternary_operator {
+    public static void main(String args[] ) {
+        String a = (5>3)?"true":"false";
+        System.out.println(a);
+    }
+    
+}
---
+++ Learn/Basics/Loops/square_pattern.java
@@ -0,0 +1,10 @@
+public class square_pattern {
+    public static void main(String args[]) {
+
+        for (int i = 0; i < 4; i++) {
+            System.out.print("****\n");
+        }
+        
+    }
+    
+}
